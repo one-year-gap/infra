e51517c72885076408109bd3e3c7d6e4bec4319a
tkv00 <tkv0098@gmail.com>
2026-02-17 17:37:29 +0900

    [HSC-95] feat: DoEnv 라이브러 추가 및 환경변수 게터 추가

--- infrastructure/src/main/java/com/myorg/config/AppConfig.java
+++ infrastructure/src/main/java/com/myorg/config/AppConfig.java
@@ -1,29 +1,59 @@
 package com.myorg.config;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 import java.util.Arrays;
 import java.util.List;
 
 public final class AppConfig {
-    public AppConfig(){}
+    public AppConfig() {
+    }
+    private static final Dotenv DOTENV = Dotenv.configure().ignoreIfMissing().load();
 
     /**
      * 가용 영역 반환
+     *
      * @return 가용 영역 string
      */
-    public static String getRegion(){
-        return System.getenv("REGION");
+    public static String getRegion() {
+        return getValue("REGION");
     }
 
     /**
      * 허용 IP 리스트 반환
+     *
      * @return 허용 IP List
      */
-    public static List<String> getAdminAllowedCidrs(){
-        String ipList = System.getenv("ADMIN_ALLOWED_CIDRS");
+    public static List<String> getAdminAllowedCidrs() {
+        String ipList = getValue("ADMIN_ALLOWED_CIDRS");
+        if (ipList == null || ipList.isBlank()) {
+            return List.of();
+        }
 
         return Arrays.stream(ipList.split(","))
                 .map(String::trim)
-                .filter(s->!s.isBlank())
+                .filter(s -> !s.isBlank())
                 .toList();
+    }
+
+    public static String getVervelIp() {
+        return getValue("VERCEL_APEX_IP");
+    }
+
+    /**
+     * Route53 도메인 이름 반환
+     * 우선순위: DOMAIN_NAME -> ROUTE53_DOMAIN_NAME -> ROOT_DOMAIN -> domainName
+     */
+    public static String getDomainName() {
+        return getValue("DOMAIN_NAME");
+    }
+
+
+    private static String getValue(String key) {
+        String v = System.getenv(key);
+        if (v != null && !v.isBlank()) return v.trim();
+        v = DOTENV.get(key);
+
+        return (v == null || v.isBlank()) ? null : v.trim();
     }
 }
